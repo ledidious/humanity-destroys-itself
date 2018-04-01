6d094db83ed1b4f16a06abd70caef7c39e9c035d
ledidious <ledidious@gmail.com>
2018-04-01 22:05:28 +0200

    Initial commit (Empty android project template)

---
+++ app/src/main/java/ledidious/org/humanity_destroys_itself/MainActivity.java
@@ -0,0 +1,13 @@
+package ledidious.org.humanity_destroys_itself;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+}
